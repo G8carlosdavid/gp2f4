497eaf5ee68f72c87f4288e74f093ed6e88e8253
G8_carlos_david_github <G8_carlos_david@proton.me>
2022-10-04 17:14:53 +0200

    "Afegint l'operació resta"

--- codi/index.php
+++ codi/index.php
@@ -1,5 +1,15 @@
-<?php
-	echo "Hola mon 2022<br>";
-	echo "Som el grup08";
-	echo "Aquesta és la primera versió de l'activitat GP2F4A6.3"
-?>
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			PÀGINA INICIAL DE L'APLICACIÓ CALCULADORA
+		</title>
+	</head>
+	<body>
+		<p><b>CALCUALDORA ARITMÈTICA</b></p>
+		<a href="suma.html">a) Suma de 2 operands</a>
+		<br>
+		<a href="resta.html">b) Resta de 2 operands</a>
+	</body>
+<html>
